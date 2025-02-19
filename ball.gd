d90e5492ed98acd45c85e063de551cc27cbaedca
luismibm <luibenmor@gmail.com>
2025-02-19 22:25:43 +0100

    blocks and more ball logic

--- ball.gd
+++ ball.gd
@@ -7,5 +7,23 @@ func _physics_process(delta: float) -> void:
 	
 	if collision:
 		
-		print(collision.get_collider().get("name")) # https://forum.godotengine.org/t/get-name-of-object-scene-during-collision/50612
-		speed = speed.bounce(collision.get_normal())
+		# https://forum.godotengine.org/t/get-name-of-object-scene-during-collision/50612
+		print(collision.get_collider().get('name'))
+		
+		if collision.get_collider().get('name') == 'player':
+			
+			var player = collision.get_collider()
+			
+			# distance, width, relation, angle, angle_rad
+			
+			speed = speed.bounce(collision.get_normal())
+			speed = speed * 1.2
+			
+			# print od the above not initialized
+			
+		elif collision.get_collider().is_in_group('blocks'):
+			collision.get_collider().queue_free()
+			speed = speed.bounce(collision.get_normal())
+			
+		else:
+			speed = speed.bounce(collision.get_normal())
